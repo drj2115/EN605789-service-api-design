eba97edf2ebf2e7d82a5ed66e4862db54491dedd
Dallas <drj2115@columbia.edu>
2022-02-13 11:28:08 -0500

    [Module3] moved json to resources folder and retrieve via URL

--- Module3/src/main/java/jh/hw/JsonApp.java
+++ Module3/src/main/java/jh/hw/JsonApp.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.StringWriter;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -15,7 +16,8 @@ public class JsonApp {
     public static void main(String[] args) throws Exception {
 
         /* Convert JSON to Object. */
-        byte[] jsonData = Files.readAllBytes(Paths.get("Module3/jh_cs_courses.json"));
+        URL url = new JsonApp().getClass().getClassLoader().getResource("jh_cs_courses.json");
+        byte[] jsonData = Files.readAllBytes(Paths.get(url.toURI()));
         ObjectMapper objectMapper = new ObjectMapper();
         Program csProgram = objectMapper.readValue(jsonData, Program.class);
         LOGGER.info("Program JSON to object\n{}", csProgram);
