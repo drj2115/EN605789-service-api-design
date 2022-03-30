acbc2cdd12157c492cdc9907530634b83ed39549
Dallas <drj2115@columbia.edu>
2022-03-30 19:07:12 -0400

    [Module10] created skeleton from Module8

---
+++ Module10/src/main/java/jh/hw/model/JsonResponse.java
@@ -0,0 +1,58 @@
+package jh.hw.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class JsonResponse {
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ssZ";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+
+    private String status;
+    private String time;
+    private String message;
+
+    public static ResponseEntity<JsonResponse> buildResponse(HttpStatus httpStatus) {
+        return buildResponse(httpStatus, httpStatus.getReasonPhrase());
+    }
+
+    public static ResponseEntity<JsonResponse> buildResponse(HttpStatus httpStatus, String message) {
+        return ResponseEntity.status(httpStatus).body(new JsonResponse(httpStatus, message));
+    }
+
+    public JsonResponse(HttpStatus httpStatus, String message) {
+        this(httpStatus, message, ZonedDateTime.now().format(DATE_TIME_FORMATTER));
+    }
+
+    public JsonResponse(HttpStatus httpStatus, String message, String time) {
+        this.status = httpStatus.toString();
+        this.message = message;
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ Module10/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Module10 - hello!</h2>
+</body>
+</html>
